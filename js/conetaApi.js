05da68937f77bb6c4a8a9104d5265b91fc902b9f
Rafaelgeomo <65200578+Rafaelgeomo@users.noreply.github.com>
2023-11-07 15:25:59 -0300

    enviar video

--- js/conetaApi.js
+++ js/conetaApi.js
@@ -3,9 +3,27 @@ async function listaVideos () { //async (difiniu como assincrona) lendo linha po
     const conexaoConvertida = await conexao.json();
     
     return conexaoConvertida; // retornar o valor da conexaoConvertida
+}
 
+async function criaVideo(titulo, descricao, url, imagem){
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST", 
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })      
+   });
+
+   const conexaoConvertida = await conexao.json();
+   return conexaoConvertida;
 }
 
 export const conectaApi = { // exportar uma variavel que vai receber um objeto cheio de funções
-    listaVideos
+    listaVideos,
+    criaVideo
 }
